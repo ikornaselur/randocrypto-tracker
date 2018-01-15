1354a159c6c8bc87612af0c28e5a6fee3a34dfb8
Axel Örn Sigurðsson <axel@takumi.com>
2018-01-15 22:52:42 +0100

    Simple task to check randocrypto status

---
+++ functions/get/index.js
@@ -0,0 +1,40 @@
+const cheerio = require('cheerio');
+const request = require('request');
+
+const bots = {
+  ikornaselur: 'https://www.randocrypto.com/bots/e596dc2840187100cce06370',
+  koddsson: 'https://www.randocrypto.com/bots/257743fd4bec7e656371dd5f',
+  mulningsvelin: 'https://www.randocrypto.com/bots/bb2728581acd3edf2aa428c4',
+  stebbib: 'https://www.randocrypto.com/bots/d77d70c16ed3bc0ea0b3c744',
+};
+
+const getStats = user => new Promise((resolve, reject) => {
+  console.log(`[getStats] getting ${user}`);
+  const url = bots[user];
+  request(url, (err, res, html) => {
+    if (!err && res.statusCode === 200) {
+      const $ = cheerio.load(html);
+      // Not optimal way of getting the value but fuck you
+      const valString = $('h2:contains("$")').text().trim().replace('$', '');
+      const value = parseFloat(valString);
+      console.log(`[getStats] got ${value} for ${user}`);
+      const result = {
+        value,
+        formattedValue: valString,
+        ROI: ((value / 1000) - 1) * 100,
+      };
+      resolve({[user]: result});
+    } else {
+      reject('Fuck if I know');
+    }
+  });
+});
+
+exports.handle = (e, ctx, cb) => {
+  const promises = Object.keys(bots).map(user => getStats(user));
+  Promise.all(promises).then(results => {
+    console.log('[main] time to combine all this shit:');
+    console.log(results);
+    cb(null, results.reduce((a, b) => Object.assign(a, b)));
+  });
+};
